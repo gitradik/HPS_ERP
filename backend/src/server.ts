49fcbb898a4999abf57cc52725e8a78149ac44c1
gitradik <radik.softdev@gmail.com>
2024-12-25 14:24:56 +0100

    BE: initial folder and files structure

---
+++ backend/src/server.ts
@@ -0,0 +1,22 @@
+import express, { Application } from 'express';
+import dotenv from 'dotenv';
+// import userRoutes from './routes/userRoutes';
+
+// Загрузка переменных окружения
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+// Создание Express-приложения
+const app: Application = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+// app.use('/api/users', userRoutes);
+
+// Запуск сервера
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
