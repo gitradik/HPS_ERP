64de605fa7bc8d4687b238b237990460a69ebc91
gitradik <radik.softdev@gmail.com>
2024-12-25 17:19:26 +0100

    sequelize psql connect

---
+++ backend/src/config/db_connect.ts
@@ -0,0 +1,10 @@
+import sequelize from './db';
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
