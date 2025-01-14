ccce8f1ba56a5cc96ebc5aa2b2a801664206712a
gitradik <radik.softdev@gmail.com>
2025-01-14 11:31:54 +0100

    added eslint.config.js

---
+++ backend/eslint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: ['eslint:recommended'],
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+    },
+    rules: {
+      // Настройте правила, которые вам нужны
+    },
+    ignores: ['node_modules', 'dist'], // Укажите директории или файлы, которые нужно игнорировать
+  };
+  
